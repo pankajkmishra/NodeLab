7e18ff757f246fdb8d31f86e2c487722f8a91b33
Pankaj K Mishra <pankajkmishra01@gmail.com>
2019-03-11 20:28:28 +0800

    created run_all_tests
    Sets up and runs all the tests in the 'demos' directory.

---
+++ run_all_tests.m
@@ -0,0 +1,24 @@
+setup 
+cd demos
+test_1C
+disp('Press any key to run the next test!')
+pause;
+clear variables; close all
+test_2R
+disp('Press any key to run the next test!')
+pause;
+clear variables; close all
+test_2Lshape
+disp('Press any key to run the next test!')
+pause;
+clear variables; close all
+test_3Lake
+disp('Press any key to run the next test!')
+pause;
+clear variables; close all
+test_4LakeIsland
+disp('All tests are complete. Press any key exit')
+pause;
+clear variables; close all
+
+cd ..
